03fac408a841011ee07cf2813f561ee5b9f2eb13
Junhyung Im <entrypointkr@gmail.com>
2022-04-30 14:44:14 +0900

    fix: inlining command algebra

--- bukkit/src/main/java/io/typecraft/command/bukkit/CommandBukkitPlugin.java
+++ bukkit/src/main/java/io/typecraft/command/bukkit/CommandBukkitPlugin.java
@@ -1,9 +1,12 @@
 package io.typecraft.command.bukkit;
 
+import io.typecraft.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Collections;
+
+import static io.typecraft.command.Command.pair;
 
 public class CommandBukkitPlugin extends JavaPlugin {
 
@@ -24,6 +27,20 @@ public class CommandBukkitPlugin extends JavaPlugin {
             // Reload!
         } else if (command instanceof MyCommand.Hello) {
             sender.sendMessage("Hello!");
+        }
+    }
+
+    // MyCommand = Reload | Hello
+    private interface MyCommand {
+        Command<MyCommand> command = Command.map(
+                pair("reload", Command.present(new MyCommand.Reload())),
+                pair("hello", Command.present(new MyCommand.Hello()))
+        );
+
+        class Reload implements MyCommand {
+        }
+
+        class Hello implements MyCommand {
         }
     }
 }
